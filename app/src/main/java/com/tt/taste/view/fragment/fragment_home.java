dd1cb87a19611470a21747f4bd3eb8fc970bf6cb
hz38957153 <haozhen38957153@sina.com>
2017-08-09 18:30:59 +0800

    添加首页fragment

---
+++ app/src/main/java/com/tt/taste/view/fragment/fragment_home.java
@@ -0,0 +1,23 @@
+package com.tt.taste.view.fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.tt.taste.R;
+
+/**
+ * Created by hz on 2017/8/9.
+ */
+
+public class fragment_home extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        return view;
+    }
+}
