c61e101fddda01b3ecd3be2a488fec8ae88f8662
hz38957153 <haozhen38957153@sina.com>
2017-08-09 20:18:30 +0800

    New changelist

--- app/src/main/java/com/tt/taste/view/fragment/Fragment_friends.java
+++ app/src/main/java/com/tt/taste/view/fragment/Fragment_friends.java
@@ -12,12 +12,12 @@ import com.tt.taste.R;
  * Created by hz on 2017/8/9.
  */
 
-public class Fragment_home extends Fragment {
+public class Fragment_friends extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_home,container,false);
+        View view = inflater.inflate(R.layout.fragment_firends,container,false);
         return view;
     }
 }
